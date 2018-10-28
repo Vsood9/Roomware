f70fbfe1118a26547e0736236af36b9facc70679
Vansh Sood <emailofvansh@gmail.com>
2018-10-28 18:32:17 +0530

    No change

---
+++ func.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  // function myMap() {
+  //   var mapOptions = {
+  //     center: new google.maps.LatLng(51.5, -0.12),
+  //     zoom: 10,
+  //     mapTypeId: google.maps.MapTypeId.HYBRID
+  // }
+  // var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  // }
+  $('.bars').click(function () {
+    $('.bar1').addClass('bar1_ac')
+    $('.bar2').addClass('bar2_ac')
+    $('.bar3').addClass('bar3_ac')
+  })
+})
